4f759f4bf9f8fe6f442ac3af52676a05275148dc
Naufal05R <naufalrabbaniihere@gmail.com>
2023-07-08 20:46:18 +0700

    Add Car Catalogue

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 
-export { Navbar, Hero, CustomButton, Footer };
+export { Navbar, Hero, CustomButton, SearchBar, CustomFilter, SearchManufacturer, Footer };
